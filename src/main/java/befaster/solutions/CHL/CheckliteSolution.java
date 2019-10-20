029280dad3675607fe114a667bfccbb12e6ed092
sbx_user1051 <sbx_user1051@169.254.1.77>
2019-10-20 16:15:55 +0000

    Sun Oct 20 16:15:55 UTC 2019

--- src/main/java/befaster/solutions/CHL/CheckliteSolution.java
+++ src/main/java/befaster/solutions/CHL/CheckliteSolution.java
@@ -11,6 +11,53 @@ public class CheckliteSolution {
     	}
     	return 0;
     }
+    
+    private Integer calculateSkusWithDiscounts(String skus){
+    	
+    }
+    
+    class SKU{
+    	
+    	private String skuName;
+    	private Integer price;
+    	private Discount discount;
+		public String getSkuName() {
+			return skuName;
+		}
+		public void setSkuName(String skuName) {
+			this.skuName = skuName;
+		}
+		public Integer getPrice() {
+			return price;
+		}
+		public void setPrice(Integer price) {
+			this.price = price;
+		}
+		public Discount getDiscount() {
+			return discount;
+		}
+		public void setDiscount(Discount discount) {
+			this.discount = discount;
+		}
+    }
+    
+    class Discount{
+    	private Integer numberOfItems;
+    	private Integer price;
+		public Integer getNumberOfItems() {
+			return numberOfItems;
+		}
+		public void setNumberOfItems(Integer numberOfItems) {
+			this.numberOfItems = numberOfItems;
+		}
+		public Integer getPrice() {
+			return price;
+		}
+		public void setPrice(Integer price) {
+			this.price = price;
+		}
+    }
 }
+
 
 
