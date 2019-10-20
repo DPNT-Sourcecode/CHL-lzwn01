eefec7b65fc1e664a13c715ec4c649860c878579
sbx_user1051 <sbx_user1051@169.254.1.77>
2019-10-20 15:03:56 +0000

    Sun Oct 20 15:03:56 UTC 2019

--- src/main/java/befaster/solutions/SUM/SumSolution.java
+++ src/main/java/befaster/solutions/SUM/SumSolution.java
@@ -1,15 +1,20 @@
 package befaster.solutions.SUM;
 
+import java.util.Optional;
+
 import befaster.runner.SolutionNotImplementedException;
 
 @SuppressWarnings("unused")
 public class SumSolution {
 
     public synchronized int compute(int x, int y) {
-    	Optional.
+    	if(x < 0 || x >= 100 || y < 0 || y >= 100){
+    		throw new IllegalArgumentException("Please input only the allowed variable values");
+    	}
         return x + y;
     }
 
 }
+
 
 
