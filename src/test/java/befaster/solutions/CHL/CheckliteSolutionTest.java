323f87f3dae9746bfe183df1f961bce413f83ec8
sbx_user1051 <sbx_user1051@169.254.1.77>
2019-10-20 16:00:55 +0000

    Sun Oct 20 16:00:55 UTC 2019

--- src/test/java/befaster/solutions/CHL/CheckliteSolutionTest.java
+++ src/test/java/befaster/solutions/CHL/CheckliteSolutionTest.java
@@ -1,5 +1,25 @@
 package befaster.solutions.CHL;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+
 public class CheckliteSolutionTest {
 
+	private CheckliteSolution solution;
+	
+    @Before
+    public void setUp() {
+
+    	solution = new CheckliteSolution();
+    }
+
+    @Test
+    public void checklite_Null() {
+        assertThat(solution.checklite(null), equalTo(-1));
+    }
 }
+
