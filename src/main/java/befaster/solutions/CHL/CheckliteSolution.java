9f19cb4fe046b6c4f411a35f350196781952e413
sbx_user1051 <sbx_user1051@169.254.1.77>
2019-10-20 16:55:53 +0000

    Sun Oct 20 16:55:53 UTC 2019

--- src/main/java/befaster/solutions/CHL/CheckliteSolution.java
+++ src/main/java/befaster/solutions/CHL/CheckliteSolution.java
@@ -1,16 +1,58 @@
 package befaster.solutions.CHL;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.common.base.Strings;
 
 import befaster.runner.SolutionNotImplementedException;
 
 public class CheckliteSolution {
+	
+	List<SKU> memorySKUs = null;
+	
+	/**
+	 * In real applications this data is available in the DB or other memory format.
+	 */
+	public CheckliteSolution(){
+		memorySKUs = new ArrayList<SKU>();
+		SKU sku1 = new SKU();
+		sku1.setSkuName("A");
+		sku1.setPrice(50);
+		Discount disc1 = new Discount();
+		disc1.setNumberOfItems(3);
+		disc1.setPrice(130);
+		sku1.setDiscount(disc1);
+		memorySKUs.add(sku1);
+		
+		SKU sku2 = new SKU();
+		sku2.setSkuName("B");
+		sku2.setPrice(30);
+		Discount disc2 = new Discount();
+		disc2.setNumberOfItems(2);
+		disc2.setPrice(45);
+		sku2.setDiscount(disc2);
+		memorySKUs.add(sku2);
+		
+		SKU sku3 = new SKU();
+		sku3.setSkuName("C");
+		sku3.setPrice(20);
+		memorySKUs.add(sku3);
+		
+		SKU sku4 = new SKU();
+		sku4.setSkuName("D");
+		sku4.setPrice(15);
+		memorySKUs.add(sku4);
+	}
+	
     public Integer checklite(String skus) {
     	if(Strings.isNullOrEmpty(skus) || !skus.equals(skus.toUpperCase())){
     		return -1;
     	}
     	return calculateSkusWithDiscounts(skus);
     }
+    
+    
     
     private Integer calculateSkusWithDiscounts(String skus){
     	return null;
@@ -58,3 +100,4 @@ public class CheckliteSolution {
 		}
     }
 }
+
